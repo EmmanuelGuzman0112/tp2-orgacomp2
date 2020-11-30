23f24c9b4735c39575541c49e9f8a31af15163cf
HSNBRUT11\Emmanuel Guzman <guzman-emmanuel@live.com>
2020-11-29 23:28:16 -0300

    Update script con pthread

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 nasm -f elf32 enmascarar_asm_externa.asm -o enmascarar_asm.o
-sudo gcc -m32  main.c enmascarar_asm.o -o main
+sudo gcc -m32  main.c enmascarar_asm.o -o main -pthread
 ./main a1.bmp a2.bmp amasc.bmp 1153x503
 ./main b1.bmp b2.bmp bmasc.bmp 3840x2160
 ./main c1.bmp c2.bmp cmasc.bmp 2560x1440
