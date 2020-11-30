913fc63b19a8917a8371967dc53eef21f60e35d2
HSNBRUT11\Emmanuel Guzman <guzman-emmanuel@live.com>
2020-11-29 23:18:36 -0300

    Actualizo script con la generacion y llamada al ejecutable

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 nasm -f elf32 enmascarar_asm_externa.asm -o enmascarar_asm.o
-sudo gcc -m32  main_c_secuencial.c enmascarar_asm.o -o ejecutable
-./ejecutable
+sudo gcc -m32  main.c enmascarar_asm.o -o main
+./main a1.bmp a2.bmp amasc.bmp 1153x503
+./main b1.bmp b2.bmp bmasc.bmp 3840x2160
+./main c1.bmp c2.bmp cmasc.bmp 2560x1440
+./main d1.bmp d2.bmp dmasc.bmp 7680x4320
+./main e1.bmp e2.bmp emasc.bmp 3840x2400
